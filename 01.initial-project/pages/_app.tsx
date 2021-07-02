2a7d6047568fbfe826e31cad905e3fca27bce4ba
Hansol Lee <hansol@nosearch.com>
2021-07-02 16:37:31 +0900

    add memberstack

--- 01.initial-project/pages/_app.tsx
+++ 01.initial-project/pages/_app.tsx
@@ -1,21 +1,27 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import React from "react";
 import { RecoilRoot } from "recoil";
 import { ReactQueryDevtools } from "react-query/devtools";
 const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
-        <ChakraProvider>
+        <ThemeManager>
           <Component {...pageProps} />
-        </ChakraProvider>
+        </ThemeManager>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </RecoilRoot>
   );
 }
+
+const ThemeManager = (props: { children: React.ReactNode }) => {
+  const theme = extendTheme({ config: {} });
+  return <ChakraProvider theme={theme}>{props.children}</ChakraProvider>;
+};
 
 export default MyApp;
---
+++ 01.initial-project/pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            src="https://api.memberstack.io/static/memberstack.js?custom"
+            data-memberstack-id="8328c6425461a71ebbf25c6e1e1d1f21"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
